2b546b309dc1c51bf8dfdff13dc5d87708f6c9bb
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-04 09:26:17 +0700

    first commit

---
+++ models/film.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const Hollywood = sequelize.define("Hollywood", {
+        ID:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nama_film: {
+            type: DataTypes.STRING,
+            allowNull: false    
+        },
+        deskripsi:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        sutradara:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        tahun_terbit:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        genre:{
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+}
